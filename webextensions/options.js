149b402527f855ea6e72706978cc929adc8d4614
YUKI Hiroshi <shimoda@clear-code.com>
2018-02-06 21:02:18 +0900

    Add configuration UI for frequency

--- webextensions/options.js
+++ webextensions/options.js
@@ -1,6 +1,7 @@
 function saveOptions(event) {
   browser.storage.local.set({
-    days: document.querySelector("#days").value || 0
+    days: document.querySelector("#days").value || 0,
+    frequency: parseInt(document.querySelector("#frequency").value)
   });
   event.preventDefault();
 }
@@ -11,7 +12,13 @@ function restoreOptions() {
   }).catch(() => {
     document.querySelector("#days").value = 0;
   });
+  browser.storage.local.get('frequency').then(({ frequency = 0 } = {}) => {
+    document.querySelector("#frequency").value = frequency;
+  }).catch(() => {
+    document.querySelector("#frequency").value = 0;
+  });
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector("#days").addEventListener("change", saveOptions);
+document.querySelector("#frequency").addEventListener("change", saveOptions);
